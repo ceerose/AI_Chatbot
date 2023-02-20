2fdcc0436313b4faad5c8f33cae59dcf4cc9412b
Chelsea Rosario <106922042+ceerose@users.noreply.github.com>
2023-02-20 12:39:24 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+"""
+Chatbot using natural language processing in neural networks in Python.
+Need to set up a so called "intense.json file"; this file contains certain categories of
+things that can happen e.g. the user greeting the chatbot.
+The chatbot can adapt to data. e.g., as the user continues to greet the bot, it'll
+begin to adapt to those phrases.
+Ultimately training the bot to recognize what the user is trying to get from it and
+then the chatbot is going to have some static responses to the users questions/requests
+"""
